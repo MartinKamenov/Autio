c6b3063ae5f477274481531fd3e823ad20695260
IvayloD <i.dinev@blubito.com>
2021-04-25 13:39:31 +0300

    added fetching and displaying of cars

--- src/components/base/AutoCompleteSearch/AutoCompleteSearch.tsx
+++ src/components/base/AutoCompleteSearch/AutoCompleteSearch.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './AutoCompleteSearch.scss';
 import { Icon } from '..';
+import { getData } from '../../../services/apiService';
+import { CarEntry } from '../../../types';
 
 // This component should be connected to backend endpoints,
 // and the onChange event's handler should be debounced
@@ -16,23 +18,27 @@ export interface autoCompleteSearchProps {
 
 const AutoCompleteSearch: React.FC<autoCompleteSearchProps> = ({
     apiWrapperUrl,
-    placeholder='',
-    className='',
-    style={}
+    placeholder = '',
+    className = '',
+    style = {}
 }) => {
     const [value, setValue] = useState('');
+    const [carEntries, setCarEntries] = useState<CarEntry[]>([]);
 
-    const fetchCarsData = (text: string) => {
+    const fetchCarsData = async (text: string): Promise<CarEntry[]> => {
+        const { data: cars } = await getData('/search', { query: text });
+        return cars;
     };
 
     useEffect(() => {
-        if(timeout) {
+        if (timeout) {
             clearTimeout(timeout);
         }
-        
-        timeout = setTimeout(() => {
+
+        timeout = setTimeout( async () => {
             //make api call
-            fetchCarsData(value);
+            const updatedCars = await fetchCarsData(value);
+            setCarEntries(updatedCars);
         }, DEBOUNCE_TIMER);
     }, [value]);
 
@@ -42,16 +48,26 @@ const AutoCompleteSearch: React.FC<autoCompleteSearchProps> = ({
                 <Icon icon='search' style={{
                     fontSize: 30,
                     color: 'gray',
-                }}/>
+                }} />
             </div>
             <input
                 value={value}
                 onChange={({ target: { value } }) => setValue(value)}
                 placeholder={placeholder}
                 style={style}
-                className={`auto-complete-input`}/>
+                className={`auto-complete-input`} />
+            <div className={'dropdown-results'}>
+                <ul>{
+                    carEntries.map(entry => (
+                        <li key={entry._id} className={'result'}>
+                            <img src={entry.imageHref}></img>
+                            <div>{entry.name}</div>
+                        </li>
+                    ))
+                }</ul>
+            </div>
         </div>
     );
 }
- 
+
 export default AutoCompleteSearch;

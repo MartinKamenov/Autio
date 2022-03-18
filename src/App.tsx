6ebd8e283b23d093a982bbb36a6ed9a8d2e9767b
Martin Kamenov <nqkvotupoime12@gmail.com>
2022-03-18 13:32:04 +0200

    Commit b

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {Provider} from 'react-redux';
 import { MainRouter } from './components/routing';
@@ -8,13 +8,19 @@ import UserProvider from './services/user/UserProvider';
 import Dialog from './components/common/Dialog';
 
 function App() {
+    const [counter, setCounter] = useState(0);
     return (
+        <>
+        {counter}
+        <button onClick={() => setCounter(counter + 1)}> Increment </button>
+        <button onClick={() => setCounter(counter - 1)}> Decrement </button>
         <Provider store={store}>
             <UserProvider>
                 <Dialog/>
                 <MainRouter/>
             </UserProvider>
         </Provider>
+        </>
     );
 }
 
